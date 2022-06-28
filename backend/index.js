720a632f4f3bb90543217801a8db72fca775ee40
Kazuki <kazuki.hirata@sun-asterisk.com>
2022-06-28 08:25:46 +0700

    update test

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3001;
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get("/api", (req, res) => {
-  res.json({ message: "Hello World!" });
+  res.json({ message: "Hello from BE!" });
 });
 
 app.get('*', (req, res) => {
