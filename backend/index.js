31185047862684dfae65e6054805fb4f72409b2b
Kazuki <kazuki.hirata@sun-asterisk.com>
2022-06-28 08:33:11 +0700

    add comment

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const path = require('path');
+
+// ① Herokuでポートが設定できるようにprocess.env.PORTを設定
 const port = process.env.PORT || 3001;
 
+// ② Reactのindex.htmlファイルを参照できるようにパスを設定
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get("/api", (req, res) => {
